7643e3ab92e2d6c4c99b71e3e1c7adc7eee03573
steven-cl <castillsteven12@gmail.com>
2025-11-11 03:18:56 -0600

    feat: configured shadcn and react bits to use components
    style: update global styles with Tailwind CSS configuration
    
    feat: update main entry point to include SplashCursor component
    
    chore: add TypeScript declarations for static assets
    
    chore: configure TypeScript for application and module resolution
    
    chore: set up Vite configuration with React and Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@/components/ui/button';
+import SplitText from '@/components/SplitText';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +20,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <SplitText text="DecodeIt" />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import SplashCursor from '@/components/SplashCursor'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SplashCursor />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
