0cb79ac570a80a52288aa7ae3a832f4f310c0c85
steven-cl <castillsteven12@gmail.com>
2025-11-13 12:42:30 -0600

    feat: add UI components for sidebar,  and mobile detection
    - Implemented useIsMobile hook for responsive design based on screen width.
    - Updated global CSS variables for sidebar theming.
    - Refactored layout to include SidebarProvider and SidebarTrigger for better structure.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="pt-10">
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <SplitText text="DecodeIt" />
-    </>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import SplashCursor from '@/components/SplashCursor'
+import Layout from './layout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SplashCursor />
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   </React.StrictMode>,
 )
